4640584a611f45d6bcaec01e59720a4690bf08d3
Dan Wilkins <dan.wilkins@stanford.edu>
2019-03-07 14:51:33 -0800

    ImagePlane class for ray tracing from an image plane/telescope

---
+++ src/raytracer/imageplane.h
@@ -0,0 +1,37 @@
+/*
+ * imagePlane.h
+ *
+ *  Created on: 25 Apr 2016
+ *      Author: drw
+ */
+
+#ifndef IMAGEPLANE_H_
+#define IMAGEPLANE_H_
+
+#include "raytracer.h"
+
+template <typename T>
+class ImagePlane : public Raytracer<T>
+{
+private:
+	T D;
+	T incl;
+	T phi0;
+
+	int Nx, Ny;
+
+	T* m_plane_x;
+	T* m_plane_y;
+
+public:
+	ImagePlane( T dist, T inc, T x0, T xmax, T dx, T y0, T ymax, T dy, T spin, T tol, T phi = 0);
+	~ImagePlane();
+
+	void InitImagePlane( T D, T incl, T phi0, T x0, T xmax, T dx, T y0, T ymax, T dy);
+
+	void RedshiftStart( );
+	void Redshift( bool projradius );
+
+};
+
+#endif /* IMAGEPLANE_H_ */
