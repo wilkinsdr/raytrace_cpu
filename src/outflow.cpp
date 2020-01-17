d816989ec2757b90a91c51356d79a36b4d612f4a
Dan Wilkins <dan.wilkins@stanford.edu>
2020-01-17 10:19:05 -0800

    outflow: Add absorption from point source continuum

--- src/outflow.cpp
+++ src/outflow.cpp
@@ -14,6 +14,21 @@ using namespace std;
 #include "raytracer/sourcetracer_imageplane.h"
 #include "include/par_file.h"
 
+bool pointsource_stop(double t, double r, double theta, double phi, double* args)
+{
+	const double a = args[0];
+	const double h = args[1];
+	const double rad = args[2];
+
+	const double x = sqrt(r*r + a*a)*sin(theta)*cos(phi);
+	const double y = sqrt(r*r + a*a)*sin(theta)*sin(phi);
+	const double z = r*cos(theta);
+
+	if( (x*x + y*y + (z-h)*(z-h)) < rad ) return true;
+
+	return false;
+}
+
 int main(int argc, char** argv)
 {
 	// parameter configuration file
@@ -47,6 +62,9 @@ int main(int argc, char** argv)
 	double source_size_z = par_file.get_parameter<double>("source_size_z");
 	double source_vel = par_file.get_parameter<double>("source_vel");
 	int source_motion = par_file.get_parameter<int>("source_motion", 1);
+	double pointsource_h = par_file.get_parameter<double>("pointsource_h");
+	double pointsource_r = par_file.get_parameter<double>("pointsource_r");
+	double tau = par_file.get_parameter<double>("tau");
 	double tol = par_file.get_parameter<double>("tol", TOL);
 
 	double dx = (xmax - x0) / (Nx - 1);
@@ -77,6 +95,48 @@ int main(int argc, char** argv)
 	}
 
 	delete RaytraceSource;
+
+	double stopping_args[] = {spin, pointsource_h, pointsource_r};
+
+	RaytraceSource = new SourceTracer_ImagePlane<double>(dist, incl, x0, xmax, dx, y0, ymax, dy, spin, en0, enmax, Nen, logbin_en, tol, plane_phi0);
+	RaytraceSource->set_source(source_size_xy, source_size_z, source_vel, source_motion);
+	RaytraceSource->RedshiftStart();
+	RaytraceSource->set_stopping_fn(pointsource_stop, stopping_args);
+	RaytraceSource->run_source_trace( 1.5*dist );
+	int* ray_status = RaytraceSource->GetStatus();
+	double *ray_t, *ray_r, *ray_theta, *ray_phi, *ray_redshift;
+	int *ray_steps;
+	RaytraceSource->MapResults(ray_steps, ray_t, ray_r, ray_theta, ray_phi, ray_redshift);
+
+	double *obs_continuum;
+	obs_continuum = new double[RaytraceSource->Nen];
+	double max_tau = 0;
+
+	int pointsource_count = 0;
+	for(int ray=0; ray<RaytraceSource->GetCount(); ray++)
+	{
+		if(ray_status[ray] == 2)
+		{
+			++pointsource_count;
+			for(int ien=0; ien<RaytraceSource->Nen; ien++)
+			{
+				if(RaytraceSource->absorb[ray][ien] > max_tau)
+					max_tau = RaytraceSource->absorb[ray][ien];
+			}
+		}
+	}
+	for(int ray=0; ray<RaytraceSource->GetCount(); ray++)
+	{
+		if(ray_status[ray] == 2)
+		{
+			for(int ien=0; ien<RaytraceSource->Nen; ien++)
+			{
+				obs_continuum[ien] += exp(-1 * RaytraceSource->absorb[ray][ien]*(tau/max_tau));
+			}
+		}
+	}
+
+	cout << pointsource_count << " rays reached pointsource" << endl;
 
 	TextOutput outfile((const char*)out_filename.c_str());
 	for(int ien=0; ien<Nen; ien++)
