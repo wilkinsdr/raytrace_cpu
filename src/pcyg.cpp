b4ed2052a6d443343e579ec9796ab23708d9f8d8
Dan Wilkins <dan.wilkins@stanford.edu>
2020-07-20 10:08:01 -0700

    pcyg: update default parameters

--- src/pcyg.cpp
+++ src/pcyg.cpp
@@ -7,16 +7,16 @@ using namespace std;
 int main()
 {
     const double rsph = 10;
-    const double rmin = 0.1;
-    const double rstar = 1;
+    const double rmin = 5;
+    const double rstar = 5;
     const double V = 0.2;
     const int Nx = 1000;
     const double dz = 0.01;
-    const double en0 = 0.1;
-    const double enmax = 1.5;
+    const double en0 = 0.8;
+    const double enmax = 1.2;
     const int Nen = 1000;
     const bool logbin_en = false;
-    const double dens = 1;
+    const double dens0 = 10;
     const double tau = 1.5;
     const double line_emis = 0.000001;
 
@@ -55,20 +55,30 @@ int main()
             for (double z = rsph; z > (-1 * rsph); z -= dz)
             {
                 const double r = sqrt(x * x + y * y + z * z);
-                double this_V = V * sqrt(r) / sqrt(rsph);
+                double this_V = V * (0.01 + (1-0.01)*(1. - 1./r));
+                //cout << setw(12) << r << setw(12) << this_V << endl;
                 const double los_v = this_V * z / sqrt(x * x + y * y + z * z);
-                const double energy = 1 + los_v;
+                //const double energy = 1 + los_v;
+                const double gamma = 1. / sqrt(1 - this_V*this_V);
+                const double costh = z / sqrt(x * x + y * y + z * z);;
+                const double energy = 1./(gamma * (1. - this_V*costh));
                 const int ien = (logbin_en) ? static_cast<int>( log(energy / en0) / log(den)) : static_cast<int>(
                         (energy - en0) / den);
+
+//                cout << setw(12) << r << setw(12) << this_V << setw(12) << los_v << setw(12) << energy << endl;
+
+                const double dens = dens0 / (r*r*abs(this_V));
 
                 if (ien >= 0 && ien < Nen)
                 {
                     if (r < rsph && r > rmin)
                     {
-                        emis[ray][ien] += (1. / (r * r)) * dz * dens * pow(energy, 3);
-                        if (z > 0) absorb[ray][ien] += dz * dens;
+                        emis[ray][ien] += (1. / (r * r)) * dz * dens * exp(-1 * absorb[ray][ien]) * pow(energy, 3);
+                        absorb[ray][ien] += dz * dens;
                     }
                 }
+
+                if(r < rstar) break;    // stop the ray if it hits the star
             }
         }
     }
@@ -96,6 +106,8 @@ int main()
         tau_total += absorb[ray0][ien];
     }
 
+    cout << "tau_total = " << tau_total << endl;
+
     double continuum_sum = 0;
 
 //    for(int ien=0; ien<Nen; ien++)
@@ -107,6 +119,8 @@ int main()
 
     for(int ien=0; ien<Nen; ien++)
         obs_continuum[ien] = 0;
+
+    int cont_rays = 0;
 
     const int istart = static_cast<int>((-rstar + rsph) / dx);
     const int iend = static_cast<int>((rstar + rsph) / dx);
@@ -121,6 +135,7 @@ int main()
             if((x*x + y*y) < (rstar*rstar))
                 for(int ien=0; ien<Nen; ien++)
                 {
+                    ++cont_rays;
                     obs_continuum[ien] += exp(-1*(tau/tau_total)*absorb[ray][ien]);
                     continuum_sum += obs_continuum[ien];
                 }
