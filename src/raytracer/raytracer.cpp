bdabc05c34306237154e2a6f782bdf8b4f7a2c53
Will Surgent <wsurgent@gmail.com>
2024-09-12 11:06:08 -0400

    changes for flat disk

--- src/raytracer/raytracer.cpp
+++ src/raytracer/raytracer.cpp
@@ -347,11 +347,11 @@ void Raytracer<T>::run_raytrace(RayDestination<T>* destination, T r_max, T rad_d
             //if(thetalim > 0 && theta + ptheta * step > thetalim) step = abs((thetalim - theta) / ptheta);
 
 
-            //if (theta + ptheta * step > M_PI_2) step = abs((M_PI_2 - theta) / ptheta);
+            if (theta + ptheta * step > M_PI_2) step = abs((M_PI_2 - theta) / ptheta);
 
-            //step = destination->step_function(r, theta, phi, step, ptheta);
+            step = destination->step_function(r, theta, phi, step, ptheta);
 
-            step = step / 30;
+            //step = step / 30;
 
             if(step < MIN_STEP) step = MIN_STEP;
 
